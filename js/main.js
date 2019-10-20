bae0d3d3f1e7ee31c4d646137f36a14dd8bc5dd7
EmilisJ <45522661+EmilisJ@users.noreply.github.com>
2019-10-20 18:53:39 +0300

    handlebars

---
+++ js/main.js
@@ -0,0 +1,28 @@
+//flagpicker in form.
+        var input = document.querySelector("#personid");
+        window.intlTelInput(input);
+
+// Progress bar.
+        var progressIcons = document.getElementById("progress-template").innerHTML;
+        var template = Handlebars.compile(progressIcons);
+        var progressData = template({ steps: [
+        {color: 'green', symbol:'&#10004;', title: 'Select service'},
+        {color: 'green', symbol:'&#10004;', title: 'Service overview'},
+        {color: 'green', symbol:'&#10004;', title: 'Billing information'},
+        {color: 'orange', symbol:'4', title: 'General information'},
+        {color: 'gray', symbol:'5', title: 'Choose signer'},
+        {color: 'gray', symbol:'6', title: 'Review'},  
+        ] });
+        document.getElementById('progressData').innerHTML += progressData;
+
+// All accounts in general info.
+        var accounts = document.getElementById("users-template").innerHTML;
+        var template = Handlebars.compile(accounts);
+        var genInfoPeople = template({ users: [
+        {name: 'Jonas', surname: 'Jonaitis', id: '1236589657895', position: 'Account', email: 'j.jonaitis@dokobit.lt', country: 'lt'},
+        {name: 'Petras', surname: 'Petraitis', id: '1236589657895', position: 'Account', email: 'p.petrulionis@dokobit.lt', country: 'ee'},
+        {name: 'Ona', surname: 'Onuliavičienė', id: '1236589657895', position: 'Account', email: 'j.onuliaviciene@dokobit.lt', country: 'is'},
+        {name: 'Jonas', surname: 'Kazimieraitis', id: '1236589657895', position: 'Account', email: 'j.jonaitis@dokobit.lt', country: 'lt'},
+        {name: 'Tomas', surname: 'Toluliavičiauskas', id: '1236589657895', position: 'Account', email: 'j.jonaitis@dokobit.lt', country: 'is'},
+        ] });
+        document.getElementById('genInfoPeople').innerHTML += genInfoPeople;
